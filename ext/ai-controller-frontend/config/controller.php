06ba53360279cce369f5d98b1f0962c76b0014fe
Thai Ngoc <ngoctt@vutatech.vn>
2020-06-25 16:29:07 +0700

    update code

---
+++ ext/ai-controller-frontend/config/controller.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'frontend' => array(
+		'basket' => array(
+			'decorators' => array(
+				'local' => array( 'Category', 'Bundle', 'Select' ),
+			),
+		),
+	),
+);
+ return array(
+	'haha'
+ );
